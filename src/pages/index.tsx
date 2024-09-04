fc1d3c200acd4dd56360b25c42746b4640034f6f
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2024-09-05 00:15:39 +0300

    feat: added nextauth client requirement from their docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
+import { NextPageContext } from "next"
+ 
+import { useSession, signIn, signOut, getSession } from "next-auth/react"
 export default function Home() {
-  return (
+  const { data: session } = useSession()
+  console.log(session)
+  if(session) {
+    return (
     <>
-      <h1>Welcome to the course</h1>
+      Signed in as {session.user.email} <br/>
+      <img src={session.user.image} alt="" />
+      <h1>Username : {session.user.name}</h1>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
-  );
+    )
+  }
+  return (
+  <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
+  )
+}
+
+export async function getServerSideProps(ctx:NextPageContext){
+  const session= await getSession(ctx);
+
+  return{
+    props:{
+      session
+    }
+  }
 }
